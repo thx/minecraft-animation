a617546b18368cf0e76754f664f76cc7614bac67
chongzhi.ldq <chongzhi.ldq@taobao.com>
2017-03-16 18:55:28 +0800

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var less = require('gulp-less');
 var path = require('path');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var deploy = require('@ali/alimama-deploy')
 
 gulp.task('less', function() {
   return gulp.src('./src/less/*.less')
@@ -19,4 +20,17 @@ gulp.task('less', function() {
 
 gulp.task('default', ['less'], function() {
   return gulp.watch('./src/less/**/*.less', ['less'])
+})
+
+
+gulp.task('daily', function() {
+    deploy.daily({
+        buildName: false //指定执行的压缩命令
+    })
+})
+
+gulp.task('publish', function() {
+    deploy.publish({
+        buildName: false //指定执行的压缩命令
+    })
 })
